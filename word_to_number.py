4b5cb8a9567408a324f30db103cac87a2baad35d
Abhay Gupta <ab18gu@gmail.com>
2020-03-05 17:49:42 -0800

    lowercased numbers and added check

--- word_to_number.py
+++ word_to_number.py
@@ -17,18 +17,20 @@ def word_to_number(word):
                      't': '8', 'u': '9', 'v': '8',
                      'w': '9', 'x': '9', 'y': '9', 'z': '9'}
 
-    word = word[4:] # delete area code
+    word = word[6:].lower() # delete area code and lowercase letters
     number = ""
 
-    for index in range(len(word)):  # iterate through word and convert each letter to a number
+    for index in range(len(word)): # iterate through word and convert each letter to a number
         p = word[index]
-        print(p)
-        number += alph_num_dict[p]
+        if p in alph_num_dict: # check if character or number
+            number += alph_num_dict[p]
+        else:
+            number += p
 
-    return(number)
+    return('1-800-' + number[:3] + '-' + number[3:])
 
 if __name__ == '__main__':
-    word = '1800painter' # input word
+    word = '1-800-painter' # input word
     number = word_to_number(word)
 
     print(number)
