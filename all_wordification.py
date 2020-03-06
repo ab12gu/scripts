04121da8d2373d5819eead62caac5a4d05eeb318
Abhay Gupta <ab18gu@gmail.com>
2020-03-05 16:00:46 -0800

    added number+word combos, however too many words

--- all_wordification.py
+++ all_wordification.py
@@ -10,6 +10,7 @@ def number_to_word(number):
 
     import enchant # English Dictionary
     from itertools import product # Cartesian Product
+    from itertools import combinations # Combination
 
     # Create a dictionary for number to letter conversion
     alph_num_dict = {'2': ('a', 'b', 'c'),
@@ -25,23 +26,32 @@ def number_to_word(number):
     word = ""
     all_words = []
     d = enchant.Dict("en_US")  # check if word is a real english word
-    perm = list(product('012', repeat=7)) # Find all combinations of numbers
+    prod = list(product('012', repeat=7)) # Find all combinations of numbers
+    comb = list(combinations([0, 1, 2, 3, 4, 5, 6], 2)) #F Find all inner combination of numbers
 
     #print(perm[0][1])
     i = 0
 
-    for i in range(len(perm)):
+    for i in range(len(prod)):
 
         for index in range(len(number)): # iterate through number and convert each number to a letter
             p = number[index]
-            word += alph_num_dict[p][int(perm[i][index])] # add letter to word
+            word += alph_num_dict[p][int(prod[i][index])] # add letter to word
 
-        if d.check(word): # add new words to list
-            all_words.append(word)
+        #if d.check(word): # add new words to list
+            #all_words.append(word)
+
+        for j in comb:
+            temp = word[j[0]:j[1]]
+            if d.check(temp):
+                temp = number[0:j[0]]+temp+number[j[1]:]
+                all_words.append(temp)
+
 
         word = ""
 
     return(all_words)
+
 if __name__ == '__main__':
     number = '18007246837' # input number
     word = number_to_word(number)
