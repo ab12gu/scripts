f451e5f83ff8171b8e784249c2baa5f373292493
ab12gu <ab18gu@gmail.com>
2026-05-04 09:44:45 -0700

    Add starter script

---
+++ law-emails/extract-law-ids.py
@@ -0,0 +1,16 @@
+# filename: extract-law-ids.py
+# 
+# author: Abhay Gupta
+# date created: 26-05-03
+
+
+def extract_ids(url):
+    print(url)
+
+
+# when a python module is imported, __name__ is set to the module's name (by defualt its __main__)
+if __name__ == '__main__':
+    url = 
+    extract_ids(url)
+
+
